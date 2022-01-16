6fdef3c8a6b07220277c1f18a84f1bb250114da9
Mateusz Korniewski <mateusz.korniewski@gmail.com>
2022-01-16 16:40:57 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Mateusz Korniewski";
+        int myAge = 30;
+        double height = 1.68;
+        boolean newToProgramming = true;
+        int actualYear = 2022;
+        String myCity = "Świdnica";
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(newToProgramming);
+        System.out.println(actualYear);
+        System.out.println(myCity);
+    }
+}
