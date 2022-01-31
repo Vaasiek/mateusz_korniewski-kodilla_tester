1f3c96a377e2933602c5fd7572b9ba3581ace09b
Mateusz Korniewski <mateusz.korniewski@gmail.com>
2022-01-31 10:33:56 +0100

    Initial Version

---
+++ kodilla-intro/src/main/java/Loops.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
+        for (int i = names.length -1; i >= 0; i--) {
+            System.out.println(names[i]);
+        }
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        System.out.println(sum);
+    }
+}
