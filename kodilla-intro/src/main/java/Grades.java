ad86a0e5b5c0e8f447fd7b0b0d33cd4f823b2552
Mateusz Korniewski <mateusz.korniewski@gmail.com>
2022-02-16 09:56:56 +0100

    Cwiczenia modul 2

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,25 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int [10];
+        this.size = 0;
+    }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+    public void lastGrade() {
+        System.out.println(this.grades[this.size]);
+    }
+
+    public void averageGrade() {
+        System.out.println(this.grades / this.size);
+    }
+}
