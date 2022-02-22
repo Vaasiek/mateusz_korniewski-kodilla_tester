397224684ea663ef9b4d6e8e6b707a61a2396c5d
Mateusz Korniewski <mateusz.korniewski@gmail.com>
2022-02-22 17:15:35 +0100

    Cwiczenia modul 2

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -4,12 +4,15 @@ public class Book {
     private String title;
 
     public static String of(String author, String title) {
-        return author;
+        return author + ", " + title;
     }
 
     static String book = Book.of("Jacek Piekara", "Cykl Inkwizytorski");
+    static String book1 = Book.of("Andrzej Sapkowski", "Wiedźmin");
 
     public static void main(String[] args) {
         System.out.println(book);
+        System.out.println((book1));
+
     }
 }
